efb367f8af8153fc80059ba52cf3b2caac7d466b
YehSal <YehiaSaleh@Yehias-MBP.home>
2016-11-24 18:20:21 -0500

    Basic homepage and routing

--- yale_athletics_app/config/routes.rb
+++ yale_athletics_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
---
+++ yale_athletics_app/db/schema.rb
@@ -0,0 +1,59 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161124224120) do
+
+  create_table "high_schools", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rosters", force: :cascade do |t|
+    t.string   "sport"
+    t.string   "season"
+    t.string   "name"
+    t.string   "college"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "bt"
+    t.string   "city"
+    t.string   "college_year"
+    t.string   "events"
+    t.string   "high_school"
+    t.string   "high_school_home_town"
+    t.string   "region_home_town"
+    t.string   "height"
+    t.string   "name"
+    t.string   "name_link"
+    t.string   "number"
+    t.string   "pos"
+    t.string   "region"
+    t.string   "season"
+    t.string   "sport"
+    t.string   "weapon"
+    t.string   "wt"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
