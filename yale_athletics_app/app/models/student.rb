1ba3b3e132e3219fcf0fed5d2fdf8aca1be2b019
YehSal <YehiaSaleh@Yehias-MacBook-Pro.local>
2016-11-28 15:24:07 -0500

    Removed unnecessary columns from Students table

--- yale_athletics_app/app/models/student.rb
+++ yale_athletics_app/app/models/student.rb
@@ -1,5 +1,16 @@
 class Student < ApplicationRecord
+  require 'byebug'
+  require 'csv'
+  # associations
   belongs_to :university
   belongs_to :roster
   belongs_to :high_school
+
+  # import data from csv
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      debugger
+      User.create! row.to_hash
+    end
+  end
 end
