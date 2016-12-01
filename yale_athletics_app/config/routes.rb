197d35d9d04c5c6d794b064883c032c7f1ca03c2
YehSal <yehia.saleh@yale.edu>
2016-12-01 11:25:53 -0500

    Fixed index display and added styling

--- yale_athletics_app/config/routes.rb
+++ yale_athletics_app/config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   get 'rosters/index', to: 'rosters#index'
   get 'rosters/import', to: 'rosters#import'
 
+  get 'students/import', to: 'students#import'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students do
-    collection { post :import }
-  end
+  # resources :students do
+  #   collection { post :import }
+  # end
 
   root 'students#index'
   get  '/help',    to: 'static_pages#help'
