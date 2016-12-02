32004e576e13777b448958c83b42747681ed1486
YehSal <yehia.saleh@yale.edu>
2016-12-02 15:02:47 -0500

    Controller searches for students based on search params

--- yale_athletics_app/app/controllers/static_pages_controller.rb
+++ yale_athletics_app/app/controllers/static_pages_controller.rb
@@ -10,4 +10,25 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def listing
+    # search params
+    @sport = params[:sports]
+    @name = params[:name]
+    @hometown = params[:hometown]
+
+    # query rosters if sport specified, otherwise get all players
+    if @sport.nil?
+      @students = Student.where(name: @name, hometown: @hometown)
+    else
+      @rosters = Roster.where(sport: @sport)
+      @students = []
+
+      @rosters.each do |roster|
+        @students << roster.players
+      end
+
+      @students.where(name: @name, hometown: @hometown)
+   end
+  end
 end
