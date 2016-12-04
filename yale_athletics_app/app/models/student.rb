8835a7f46ffa908c969d3099f76c433a6d2588a1
Yehia Saleh <YehiaSaleh@Yehias-MBP.wireless.yale.internal>
2016-12-04 11:11:34 -0500

    more debugging

--- yale_athletics_app/app/models/student.rb
+++ yale_athletics_app/app/models/student.rb
@@ -18,7 +18,7 @@ class Student < ApplicationRecord
   def self.import
     csv_text = File.read('../data/processed/player_bios_processed.csv')
 
-    csv = CSV.parse(csv_text, headers: true :row_sep => :auto, :col_sep => ";")
+    csv = CSV.parse(csv_text, headers: true)
     csv.each do |row|
       Student.create!(:student_id => row[0], :name => row[1], :city => row[2],
                       'high school'.to_sym => row[3], :hometown => row[4],
