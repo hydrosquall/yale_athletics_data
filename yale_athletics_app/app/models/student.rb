67002123103271ed950bb2dd3d2e8a6fbd074fdc
Yehia Saleh <YehiaSaleh@Yehias-MBP.wireless.yale.internal>
2016-12-04 11:27:55 -0500

    updated student model

--- yale_athletics_app/app/models/student.rb
+++ yale_athletics_app/app/models/student.rb
@@ -10,22 +10,22 @@ class Student < ApplicationRecord
   validates :name, presence: true
   # deny special characters
   # validates_format_of :name, with: /^[a-zA-Z]+$/
-  validates :college, presence: true
-  validates_inclusion_of :college, in: %w(Yale Harvard),
-                                   allow_nil: false
+  # validates :college, presence: true
+  # validates_inclusion_of :college, in: %w(Yale Harvard),
+  #                                  allow_nil: false
 
   # imports csv file into db
   def self.import
     csv_text = File.read('../data/processed/player_bios_processed.csv')
+
     csv = CSV.parse(csv_text, headers: true)
     csv.each do |row|
-      Student.create!(:student_id => row[0], :name => row[1], :city => row[2],
-                      'high school'.to_sym => row[3], :hometown => row[4],
-                      'ht.'.to_sym => row[5], 'no.'.to_sym => row[6],
-                      :position => row[7], :region => row[8],
-                      'wt.'.to_sym => row[9],
-                      'active seasons'.to_sym => row[10], :misc => row[11],
-                      :bio => row[12], :college => row[13])
+      Student.create!(:student_id => row[0], :name => row[1], :hometown => row[2],
+                      'ht.'.to_sym => row[3], 'no.'.to_sym => row[4],
+                      :position => row[5], 'wt.'.to_sym => row[6],
+                      :misc => row[7], :bio => row[8], :college => row[9],
+                      :startseason => row[10], :endseason => row[11],
+                      :major => row[12])
     end
   end
 end
--- yale_athletics_app/db/schema.rb
+++ yale_athletics_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202213500) do
+ActiveRecord::Schema.define(version: 20161203224659) do
 
   create_table "high_schools", force: :cascade do |t|
     t.datetime "created_at",     null: false
